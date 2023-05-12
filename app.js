0ba2379e8878e7c56b53d1745678b7ef48849fd8
Muhammadhamza125 <121334021+Muhammadhamza125@users.noreply.github.com>
2023-05-12 19:49:01 +0500

    Todo List

---
+++ app.js
@@ -0,0 +1,8 @@
+var input = document.getElementById("inputTodo")
+var addBtn = document.getElementById("addBtn")
+
+
+addBtn.addEventListener("click", function(){
+    input = inputTodo.value;
+    var lsting ="";
+})
